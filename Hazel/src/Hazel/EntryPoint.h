e494073ca071a3f2486a1865b83b3352ccda8795
jxwu <1463009704@qq.com>
2023-11-21 23:13:40 +0800

    logginh

--- Hazel/src/Hazel/EntryPoint.h
+++ Hazel/src/Hazel/EntryPoint.h
@@ -6,6 +6,11 @@ extern Hazel::Application* Hazel::CreateApplication();
 
 int main(int argc, char** argv)
 {
+	Hazel::Log::Init();
+	HZ_CORE_WARN("Init logging");
+	int a = 5;
+	HZ_INFO("the value a = {}", a);
+
 	printf("Hazel Engine!");
 	auto app = Hazel::CreateApplication();
 	app->run();
