299ef3e022b6c9fe6856f24abebcb91734baf2a8
jxwu <1463009704@qq.com>
2023-11-23 23:27:17 +0800

    windw abstarction, GLFW and window event

--- Hazel/src/Hazel/Application.cpp
+++ Hazel/src/Hazel/Application.cpp
@@ -3,33 +3,40 @@
 
 
 
-#include "Hazel/Events/ApplicationEvent.h"
 #include "Hazel/Log.h"
 
-namespace Hazel {
-	Application::Application() {
+#include <GLFW/glfw3.h>
 
+namespace Hazel {
+#define BIND_EVENT_FN(x) std::bind(&Application::x, this, std::placeholders::_1) //functional feature£¡
+	Application::Application() {
+		m_Window = std::unique_ptr<Window>(Window::Create());
+		m_Window->SetEventCallback(BIND_EVENT_FN(OnEvent)); 
 	}
 	Application::~Application() 
 	{
 	
 	}
+	void Application::OnEvent(Event& e)
+	{
+		EventDispatcher dispatcher(e);
+		dispatcher.Dispatch<WindowCloseEvent>(BIND_EVENT_FN(OnWindowClose));  //dispatch the func!!
+
+		HZ_CORE_TRACE("{0}", e);
+	}
 	void Application::run()
 	{
 
-		WindowResizeEvent e(1280, 720);
-		if (e.IsInCategory(EventCategoryApplication))
+		while (m_Running)
 		{
-			HZ_TRACE(e);
+			glClearColor(1, 0, 1, 1);
+			glClear(GL_COLOR_BUFFER_BIT);
+			m_Window->OnUpdate();
 		}
-		if (e.IsInCategory(EventCategoryInput))
-		{
-			HZ_TRACE(e);
-		}
-
-		while (true)
-		{
-
-		}
+	}
+	bool Application::OnWindowClose(WindowCloseEvent& e)
+	{
+		m_Running = false;
+		return true;
 	}
 }
--- Hazel/src/Hazel/Application.h
+++ Hazel/src/Hazel/Application.h
@@ -3,6 +3,8 @@
 #include "Core.h"
 
 #include "Events/Event.h"
+#include "Hazel/Events/ApplicationEvent.h"
+#include "Window.h"
 
 
 namespace  Hazel {
@@ -12,6 +14,12 @@ namespace  Hazel {
 		Application();
 		virtual ~Application();
 		void run();
+		void OnEvent(Event& e);
+
+	private:
+		bool OnWindowClose(WindowCloseEvent& e);
+		std::unique_ptr<Window> m_Window;
+		bool m_Running = true;
 	};
 
 	// To be defined in CLIENT
